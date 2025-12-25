7506fd2485987d346a00d8789f3fbf36fca618b0
Lhhung <lehiephung199@gmail.com>
2025-12-25 23:04:50 +0700

    update admin_home

--- src/Pages/Admin/AdminHome/AdminHome.jsx
+++ src/Pages/Admin/AdminHome/AdminHome.jsx
@@ -1,5 +1,6 @@
 import StatsGrid from '../../../Components/Admin/StatsGrid/StatsGrid';
 import AdminCustomers from '../../../Components/Admin/AdminUsers/AdminCustomers/AdminCustomers';
+import AdminTechnicians from '../../../Components/Admin/AdminUsers/AdminTechnicians/AdminTechnicians';
 import { Fragment } from 'react/jsx-runtime';
 
 export default function AdminHome({ active }) {
@@ -7,6 +8,7 @@ export default function AdminHome({ active }) {
         <Fragment>
             {active === 'dashboard' && <StatsGrid />}
             {active === 'users-customer' && <AdminCustomers />}
+            {active === 'users-technician' && <AdminTechnicians />}
         </Fragment>
     );
 }
